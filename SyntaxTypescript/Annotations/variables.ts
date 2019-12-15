5893614e239e3bde6094c1928ee572d11ec43ede
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2019-12-14 22:00:21 -0300

    Tipos y Syntax de Tipos

---
+++ SyntaxTypescript/Annotations/variables.ts
@@ -0,0 +1,10 @@
+const apples: number = 5;
+let speed: string = 'fast';
+
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined;
+console.log(nothing);
+
+// Built in Objects
+let now: Date = new Date();
