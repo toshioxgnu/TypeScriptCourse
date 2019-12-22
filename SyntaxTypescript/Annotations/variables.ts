fb38561b3332ed61f4519b0f2b2e428f23009dba
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2019-12-21 22:08:22 -0300

    Avanzando en tipos

--- SyntaxTypescript/Annotations/variables.ts
+++ SyntaxTypescript/Annotations/variables.ts
@@ -8,3 +8,28 @@ console.log(nothing);
 
 // Built in Objects
 let now: Date = new Date();
+
+// array
+
+let colors: string[] = ['red', 'green', 'blue'];
+console.log(colors[1]);
+
+// classes
+class Car {}
+
+let car: Car = new Car();
+
+// objeto literal
+let point: {
+	x: number;
+	y: number;
+} = {
+	x: 5,
+	y: 20
+};
+
+// function
+
+const logNumber: (i: number) => void = (i: number) => {
+	console.log(i);
+};
